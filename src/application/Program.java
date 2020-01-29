f0b715819c49d4a05c9d2e09215128306633e346
Wagner Witt <wittwizard@gmail.com>
2020-01-29 17:18:33 -0300

    05.02 Vetor (com objetos)

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,30 +3,35 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class Program {
 	public static void main(String[] args) {
-		
-		// 05.01 Vetor
+
+		// 05.02 Vetores (com objetos)
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		double[] vect = new double[n];
-		
-		for (int i = 0; i < n; i++) {
-			vect[i] = sc.nextDouble();
+		Product[] product = new Product[n];
+
+		for (int count = 0; count < product.length; count++) {
+			sc.nextLine();
+			String name = sc.nextLine();
+			double price = sc.nextDouble();
+			product[count] = new Product(name, price);
 		}
-		
+
 		double sum = 0.0;
-		
-		for (int i = 0; i < n; i++) {
-			sum += vect[i];
+
+		for (int count = 0; count < product.length; count++) {
+			sum += product[count].getPrice();
 		}
-		
+
 		double avg = sum / n;
-		
-		System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
-		
+
+		System.out.printf("AVERAGE PRICE = %.2f", avg);
+
 		sc.close();
 	}
 }
