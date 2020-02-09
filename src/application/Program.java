47530eae4017723dc2b1fbe614a10bb5751f28db
Wagner Witt <wittwizard@gmail.com>
2020-02-09 11:51:02 -0300

    05.08 Matriz

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,65 +5,50 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 
-		// 05.07 Matriz
+		// 05.08 Matriz
 
-//		Fazer um programa para ler um número inteiro N e uma matriz de
-//		ordem N contendo números inteiros. Em seguida, mostrar a diagonal
-//		principal e a quantidade de valores negativos da matriz.
+//		Fazer um programa para ler dois números inteiros M e N, e depois ler
+//		uma matriz de M linhas por N colunas contendo números inteiros,
+//		podendo haver repetições. Em seguida, ler um número inteiro X que
+//		pertence à matriz. Para cada ocorrência de X, mostrar os valores à
+//		esquerda, acima, à direita e abaixo de X, quando houver, conforme
+//		exemplo.
 
 		Scanner sc = new Scanner(System.in);
+		
+		int m = sc.nextInt();
 		int n = sc.nextInt();
+		int[][] mat = new int[m][n];
 
-		int[][] mat = new int[n][n];
-
-		int negativeNumbers = 0;
 		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat.length; j++) {
+			for (int j = 0; j < mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
-				if (mat[i][j] < 0) {
-					negativeNumbers++;
+			}
+		}
+
+		int target = sc.nextInt();
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == target) {
+					System.out.println("Position " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Left: " + mat[i][j - 1]);
+					}
+					if (i > 0) {
+						System.out.println("Up: " + mat[i - 1][j]);
+					}
+					if (j < mat[i].length - 1) {
+						System.out.println("Right: " + mat[i][j + 1]);
+					}
+					if (i < mat.length - 1) {
+						System.out.println("Down: " + mat[i +1][j]);
+					}
+
 				}
 			}
 		}
 
-		System.out.println("Main diagonal:");
-		for (int i = 0; i < mat.length; i++) {
-			System.out.print(mat[i][i] + " ");
-		}
-
-		System.out.println();
-		System.out.println("Negative numbers = " + negativeNumbers);
-
 		sc.close();
-
-		// Resolução alternativa
-//		Scanner sc = new Scanner(System.in);
-//
-//		int n = sc.nextInt();
-//		int[][] mat = new int[n][n];
-//
-//		for (int i = 0; i < mat.length; i++) {
-//			for (int j = 0; j < mat[i].length; j++) {
-//				mat[i][j] = sc.nextInt();
-//			}
-//		}
-//
-//		System.out.println("Main diagonal:");
-//		for (int i = 0; i < mat.length; i++) {
-//			System.out.print(mat[i][i] + " ");
-//		}
-//		System.out.println();
-//
-//		int count = 0;
-//		for (int i = 0; i < mat.length; i++) {
-//			for (int j = 0; j < mat[i].length; j++) {
-//				if (mat[i][j] < 0) {
-//					count++;
-//				}
-//			}
-//		}
-//		System.out.println("Negative numbers = " + count);
-//
-//		sc.close();
 	}
 }
