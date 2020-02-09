e858a40b89d2fcf2c23fcb308814bd6e59144e51
Wagner Witt <wittwizard@gmail.com>
2020-02-09 11:18:03 -0300

    05.06 Matriz

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,63 +1,69 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Employee;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 05.06 List (exercicio)
+		// 05.07 Matriz
 
-//		Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario) de
-//		N funcionários. Não deve haver repetição de id.
-//		Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário.
-//		Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma
-//		mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionários,
-//		conforme exemplos.
-//		Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa
-//		ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de
-//		aumento por porcentagem dada.
+//		Fazer um programa para ler um número inteiro N e uma matriz de
+//		ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+//		principal e a quantidade de valores negativos da matriz.
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 
-		List<Employee> list = new ArrayList<>();
+		int[][] vect = new int[n][n];
 
-		System.out.print("How many employees will be registraded? ");
-		int numb = sc.nextInt();
-		System.out.println();
-		for (int count = 0; count < numb; count++) {
-			System.out.println("Employee #" + (count + 1) + ":");
-			System.out.print("Id: ");
-			int id = sc.nextInt();
-			System.out.print("Name: ");
-			sc.nextLine();
-			String name = sc.nextLine();
-			System.out.print("Salary: ");
-			double salary = sc.nextDouble();
-			list.add(new Employee(id, name, salary));
-			System.out.println();
+		int negativeNumbers = 0;
+		for (int i = 0; i < vect.length; i++) {
+			for (int j = 0; j < vect.length; j++) {
+				vect[i][j] = sc.nextInt();
+				if (vect[i][j] < 0) {
+					negativeNumbers++;
+				}
+			}
 		}
 
-		System.out.print("Enter the employee id that will have salary increase: ");
-		int id = sc.nextInt();
-		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
-		if (emp == null) {
-			System.out.println("This id does not exist!");
-		} else {
-			System.out.print("Enter the percentage: ");
-			double percentage = sc.nextDouble();
-			emp.increaseSalary(percentage);
+		System.out.println("Main diagonal:");
+		for (int i = 0; i < vect.length; i++) {
+			System.out.print(vect[i][i] + " ");
 		}
 
 		System.out.println();
-		System.out.println("List of employees:");
-		for (Employee obj : list) {
-			System.out.println(obj);
-		}
+		System.out.println("Negative numbers = " + negativeNumbers);
+
+		sc.close();
+
+		// Resolução alternativa
+//		Scanner sc = new Scanner(System.in);
+//
+//		int n = sc.nextInt();
+//		int[][] mat = new int[n][n];
+//
+//		for (int i = 0; i < mat.length; i++) {
+//			for (int j = 0; j < mat[i].length; j++) {
+//				mat[i][j] = sc.nextInt();
+//			}
+//		}
+//
+//		System.out.println("Main diagonal:");
+//		for (int i = 0; i < mat.length; i++) {
+//			System.out.print(mat[i][i] + " ");
+//		}
+//		System.out.println();
+//
+//		int count = 0;
+//		for (int i = 0; i < mat.length; i++) {
+//			for (int j = 0; j < mat[i].length; j++) {
+//				if (mat[i][j] < 0) {
+//					count++;
+//				}
+//			}
+//		}
+//		System.out.println("Negative numbers = " + count);
+//
+//		sc.close();
 	}
 }
