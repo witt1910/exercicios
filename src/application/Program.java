6612e48f1d2cc7667ad7e170d9e57e1976d9cfd2
Wagner Witt <wittwizard@gmail.com>
2020-02-09 12:03:05 -0300

    07.01 Emun (com conversão de String para Enum)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,54 +1,25 @@
 package application;
 
-import java.util.Scanner;
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 05.08 Matriz
+		// 07.01 Emun (com conversão de String para Enum)
 
-//		Fazer um programa para ler dois números inteiros M e N, e depois ler
-//		uma matriz de M linhas por N colunas contendo números inteiros,
-//		podendo haver repetições. Em seguida, ler um número inteiro X que
-//		pertence à matriz. Para cada ocorrência de X, mostrar os valores à
-//		esquerda, acima, à direita e abaixo de X, quando houver, conforme
-//		exemplo.
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 
-		Scanner sc = new Scanner(System.in);
-		
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		int[][] mat = new int[m][n];
+		System.out.println(order);
 
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat[i].length; j++) {
-				mat[i][j] = sc.nextInt();
-			}
-		}
+		OrderStatus os1 = OrderStatus.DELIVERED;
 
-		int target = sc.nextInt();
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
 
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat[i].length; j++) {
-				if (mat[i][j] == target) {
-					System.out.println("Position " + i + "," + j + ":");
-					if (j > 0) {
-						System.out.println("Left: " + mat[i][j - 1]);
-					}
-					if (i > 0) {
-						System.out.println("Up: " + mat[i - 1][j]);
-					}
-					if (j < mat[i].length - 1) {
-						System.out.println("Right: " + mat[i][j + 1]);
-					}
-					if (i < mat.length - 1) {
-						System.out.println("Down: " + mat[i +1][j]);
-					}
+		System.out.println(os1);
+		System.out.println(os2);
 
-				}
-			}
-		}
-
-		sc.close();
 	}
 }
