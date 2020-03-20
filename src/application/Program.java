a56814ae7b66c113ba10627976c1d79d53476bcb
Wagner Witt <wittwizard@gmail.com>
2020-03-20 13:26:04 -0300

    07.04 Composição e StringBuilder

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,33 +2,72 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
-import entities.Comment;
-import entities.Post;
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
 
-		// 07.03 Composição e StringBuilder
+		// 07.04 Composição e StringBuilder
+
+		// Ler os dados de um pedido com N itens (N fornecido pelo usuário).
+		// Depois, mostrar um sumário do pedido conforme exemplo (próxima página).
+		// Nota: o instante do pedido deve ser o instante do sistema: new Date()
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		
+		Client client = new Client(name, email, birthDate);
 
-		Comment c1 = new Comment("Have a nice trip!");
-		Comment c2 = new Comment("Wow that's awesome!");
-		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), 
-				"Traveling to New Zealand",
-				"I'm going to visit this wonderful country", 12);
-		p1.addComment(c1);
-		p1.addComment(c2);
-
-		Comment c3 = new Comment("Good night");
-		Comment c4 = new Comment("May the force be with you");
-		Post p2 = new Post(sdf.parse("28/07/2018 23:14:19"), 
-				"Good night, guys", "See you tomorrow", 5);
-		p2.addComment(c3);
-		p2.addComment(c4);
-
-		System.out.println(p1);
-		System.out.println(p2);
+		System.out.println();
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(new Date(), status, client);
+		
+		System.out.print("How many items to this order? ");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			System.out.println("Enter #" + i + " item data: ");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			
+			Product product = new Product(productName, productPrice);
+			
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+			
+			OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		System.out.println("ORDER SUMARY:");
+		System.out.println(order);
+		
+		
+		sc.close();
 	}
 }
---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package entities.enums;
+
+public enum OrderStatus {
+	DELIVERED,
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED
+}
