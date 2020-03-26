07103edf195c151021b5f625d18c26cb93a78ccd
Wagner Witt <wittwizard@gmail.com>
2020-03-26 15:16:27 -0300

    11.01 File e Scanner

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,30 @@
 package application;
 
-import java.util.Locale;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
-
-import model.entities.Account;
-import model.exceptions.DomainException;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 09.07 Exceções personalizadas - exercício
-
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter account data");
-		System.out.print("Number: ");
-		int number = sc.nextInt();
-		System.out.print("Holder: ");
-		sc.nextLine();
-		String holder = sc.nextLine();
-		System.out.print("Initial balance: ");
-		double balance = sc.nextDouble();
-		System.out.print("Withdraw limit: ");
-		double withdrawLimit = sc.nextDouble();
-
-		Account account = new Account(number, holder, balance, withdrawLimit);
-
-		System.out.println();
-		System.out.print("Enter amount for withdraw: ");
-		double amount = sc.nextDouble();
+		// 11.01 File e Scanner
+		
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
 		try {
-			account.withdraw(amount);
-			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
-		} catch (DomainException e) {
-			System.out.println("Withdraw error: " + e.getMessage());
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
 		}
-		sc.close();
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
 	}
 }
