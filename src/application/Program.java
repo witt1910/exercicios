3d850710c224429ffbad1e86299ef2137e73c6ca
Wagner Witt <wittwizard@gmail.com>
2020-03-26 17:06:20 -0300

    11.06 Obter informações do caminho do arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 
-		// 11.05 Manipular pastas com File	\
+		// 11.06 Obter informações do caminho do arquivo
 
 		Scanner sc = new Scanner(System.in);
 		
@@ -14,24 +14,14 @@ public class Program {
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
+
+		// Obter somente nome e tipo
+		System.out.println("getName: " + path.getName());
+		// Obter somente o diretório do arquivo
+		System.out.println("getParent: " + path.getParent());
+		// Obter direiório e nome do arquivo, com tipo
+		System.out.println("getPath: " + path.getPath());
 		
-		// Listar somente pastas
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for(File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		// Listar somente arquivos
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for(File file : files) {
-			System.out.println(file);
-		}
-		
-		// Criar pasta (e confirmar a sua criação)
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created successfully: " + success);
 		sc.close();
 	}
 }
