a702a477b0ff9f9477305c49c34267ddafa28ddf
Wagner Witt <wittwizard@gmail.com>
2020-03-26 16:09:46 -0300

    11.03 Bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,35 +7,21 @@ import java.io.IOException;
 public class Program {
 	public static void main(String[] args) {
 
-		// 11.02 FileReader e BufferedReader (não otimizada)
-
-		String path = "C:\\temp\\tin.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
-
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		// 11.03 Bloco try-with-resources
+		
+		String path = "C:\\temp\\in.txt";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
 			String line = br.readLine();
-
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
+		catch (IOException e) {
+			e.getMessage();
+		}
+
 	}
 }
