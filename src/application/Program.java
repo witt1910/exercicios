3a5f780447b5b12760b631496318fb48a61abdc3
Wagner Witt <wittwizard@gmail.com>
2020-03-26 16:22:50 -0300

    11.04 FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,26 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 11.03 Bloco try-with-resources
-		
-		String path = "C:\\temp\\in.txt";
-		
-		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
-			String line = br.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-		}
-		catch (IOException e) {
-			e.getMessage();
-		}
+		// 11.04 FileWriter e BufferedWriter
 
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+
+		String path = "C:\\temp\\out.txt";
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
