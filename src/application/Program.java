dcf34060e4c1bf6c2eceefbf853b720354acccb9
Wagner Witt <wittwizard@gmail.com>
2020-03-30 13:34:07 -0300

    13.04 Interfaces - Exercício

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,60 +6,55 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-import model.entities.CarRental;
-import model.entities.Vehicle;
-import model.services.BrazilTaxService;
-import model.services.RentalServices;
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
 
-		// 13.03 Interfaces - Inversão de controle, Injeção de dependências (por meio de construtor)
+		// 13.04 Interfaces - Exercício
 
-//		Uma locadora brasileira de carros cobra um valor por hora para locações de até
-//		12 horas. Porém, se a duração da locação ultrapassar 12 horas, a locação será
-//		cobrada com base em um valor diário. Além do valor da locação, é acrescido no
-//		preço o valor do imposto conforme regras do país que, no caso do Brasil, é 20%
-//		para valores até 100.00, ou 15% para valores acima de 100.00. Fazer um
-//		programa que lê os dados da locação (modelo do carro, instante inicial e final da
-//		locação), bem como o valor por hora e o valor diário de locação. O programa
-//		deve então gerar a nota de pagamento (contendo valor da locação, valor do
-//		imposto e valor total do pagamento) e informar os dados na tela. Veja os
-//		exemplos.
-		
+//		Uma empresa deseja automatizar o processamento de seus contratos. O processamento de
+//		um contrato consiste em gerar as parcelas a serem pagas para aquele contrato, com base no
+//		número de meses desejado.
+//		A empresa utiliza um serviço de pagamento online para realizar o pagamento das parcelas.
+//		Os serviços de pagamento online tipicamente cobram um juro mensal, bem como uma taxa
+//		por pagamento. Por enquanto, o serviço contratado pela empresa é o do Paypal, que aplica
+//		juros simples de 1% a cada parcela, mais uma taxa de pagamento de 2%.
+//		Fazer um programa para ler os dados de um contrato (número do contrato, data do contrato,
+//		e valor total do contrato). Em seguida, o programa deve ler o número de meses para
+//		parcelamento do contrato, e daí gerar os registros de parcelas a serem pagas (data e valor),
+//		sendo a primeira parcela a ser paga um mês após a data do contrato, a segunda parcela dois
+//		meses após o contrato e assim por diante. Mostrar os dados das parcelas na tela.
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		
-		System.out.println("Enter rental data");
-		System.out.print("Car model: ");
-		String carModel = sc.nextLine();
-		System.out.print("Pickup (dd/MM/yyyy HH:mm): ");
-		Date start = sdf.parse(sc.nextLine());
-		System.out.print("Return (dd/MM/yyyy HH:mm): ");
-		Date finish= sdf.parse(sc.nextLine());	
-		
-		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
-		
-		System.out.print("Enter price per hour: ");
-		double pricePerHour = sc.nextDouble();
-		System.out.print("Enter price per day: ");
-		double pricePerDay = sc.nextDouble();
-		
-		// INVERSÃO DE CONTROLE - a classe principal trata a especificidade, enquanto a classe
-		// 		RentalService apenas aplica a abstração 'TaxService'.
-		//		Com isso se retira da classe a  responsabilidade de instaciar suas dependências.
-		// INJEÇÃO DE DEPENDÊNCIA - é no construtor da classe no main que ocorre a instanciação
-		// 		específica do 'BrazilTaxService', e a classe RentalService só casa com a sua generalidade.
-		//		Um componente externo instancia a dependência, que então é injetada no objeto 'pai'.
-		RentalServices rentalService = new RentalServices(pricePerHour, pricePerDay, new BrazilTaxService());
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		rentalService.processInvoice(cr);
-		
-		System.out.println("INVOICE:");
-		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
-		System.out.println("Taz: " + String.format("%.2f", cr.getInvoice().getTax()));
-		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+		System.out.println("Enter contract data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Date (dd/MM/yyyy): ");
+		Date date = sdf.parse(sc.next());
+		System.out.print("Contract value: ");
+		double totalValue = sc.nextDouble();
+
+		Contract contract = new Contract(number, date, totalValue);
+
+		System.out.print("Enter number of installments: ");
+		int numbInstallment = sc.nextInt();
+
+		ContractService contractService = new ContractService(new PaypalService());
+
+		contractService.processContract(contract, numbInstallment);
+
+		System.out.println("Installments:");
+		for (Installment x : contract.getInstallments()) {
+			System.out.println(x);
+		}
+
 		sc.close();
 	}
 }
