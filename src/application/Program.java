649ca3873cac0df61141756d0c6093a2cd4231a3
Wagner Witt <wittwizard@gmail.com>
2020-03-30 16:03:24 -0300

    13.10 Interfaces/Comparable - Exercício 04 - comparação entre objetos (com Comparable)

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,26 +7,32 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import model.entities.Employee;
+
 public class Program {
 	public static void main(String[] args) {
 
-		// 13.09 Interfaces/Comparable - Exercício 04 - comparação entre Strings
+		// 13.10 Interfaces/Comparable - Exercício 04 - comparação entre objetos (com Comparable)
 
-		List<String> list = new ArrayList<>();
+		List<Employee> list = new ArrayList<>();
 		String path = "C:\\temp\\in.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				String name = fields[0];
+				double salary = Double.parseDouble(fields[1]);
+				Employee employee = new Employee(name, salary);
+				list.add(employee);
+				employeeCsv = br.readLine();
 			}
 			
 			Collections.sort(list);
 			
-			for(String s : list) {
-				System.out.println(s);
+			for(Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
 			}
 
 		} catch (IOException e) {
