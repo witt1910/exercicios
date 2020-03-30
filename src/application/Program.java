e20ad87978976724a0ad267e85daed7405d2d9b3
Wagner Witt <wittwizard@gmail.com>
2020-03-30 14:51:00 -0300

    13.06 Interfaces- Exercício 02 (com interface e classe abstrata)

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,21 +2,21 @@ package application;
 
 import model.entities.Circle;
 import model.entities.Rectangle;
-import model.entities.Shape;
+import model.entities.AbstractShape;
 import model.enums.Color;
 
 public class Program {
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 
-		// 13.05 Interfaces- Exercício 02 (sem interface, apens com classe abstrata)
+		// 13.06 Interfaces- Exercício 02 (com interface e classe abstrata)
 
-		Shape s1 = new Circle(Color.BLACK, 2.0);
-		Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
-		
+		AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+
 		System.out.println("Circle color: " + s1.getColor());
 		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
 		System.out.println("Rectangle color: " + s2.getColor());
 		System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
-		
+
 	}
 }
