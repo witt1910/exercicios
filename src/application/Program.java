7bb61aacb0f5dec4d18acd94fc8b6cb433be85e1
Wagner Witt <wittwizard@gmail.com>
2020-03-31 11:50:06 -0300

    14.01 Generics

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,34 @@
 package application;
 
-import java.util.Locale;
 import java.util.Scanner;
 
-import model.services.InterestService;
-import model.services.UsaInterestService;
+import model.services.PrintService;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 13.11 Interfaces - Default methods
+		// 14.01 Generics
 
-//		Fazer um programa para ler uma quantia e a duração em meses de um
-//		empréstimo. Informar o valor a ser pago depois de decorrido o prazo do
-//		empréstimo, conforme regras de juros do Brasil. A regra de cálculo de
-//		juros do Brasil é juro composto padrão de 2% ao mês.
-//		Depois, inserir o mesmo, com um exemplo norteamericano, considerando
-//		juro de 1% ao mês.
-
-		Locale.setDefault(Locale.US);
+//		Deseja-se fazer um programa que leia uma quantidade N, e depois N números
+//		inteiros. Ao final, imprima esses números de forma organizada conforme
+//		exemplo. Em seguida, informar qual foi o primeiro valor informado.
+		
 		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Amount: ");
-		double amount = sc.nextDouble();
-		System.out.print("Months: ");
-		int months = sc.nextInt();
-
-		InterestService is = new UsaInterestService(1.0);
-		double payment = is.payment(amount, months);
-
-		System.out.println("Payment afeter: " + months + " months:");
-		System.out.println(String.format("%.2f", payment));
+		
+		PrintService<Integer> ps = new PrintService<>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		Integer x = ps.first();
+		System.out.println("First: " + x);
+		
+		sc.close();
 	}
 }
