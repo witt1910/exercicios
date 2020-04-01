2daf18c67d0569c7d7f5706e1909d69d4cafb278
Wagner Witt <wittwizard@gmail.com>
2020-04-01 12:11:06 -0300

    14.03 Generics - Tipos curinga (wildcard types)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,55 +1,21 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-
-import model.entities.Product;
-import model.services.CalculationService;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.02 Genericos delimitados
+		// 14.03 Generics - Tipos curinga (wildcard types)
+		// O supertipo de qualquer tipo de lista é List<?> - e não List<Object>
+		
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+	}
 
-//		Uma empresa de consultoria deseja avaliar a performance de produtos,
-//		funcionários, dentre outras coisas. Um dos cálculos que ela precisa é encontrar
-//		o maior dentre um conjunto de elementos. Fazer um programa que leia um
-//		conjunto de produtos a partir de um arquivo, conforme exemplo, e depois
-//		mostre o mais caro deles.
-
-//		in.txt
-//		Computer,890.50
-//		IPhone X,910.00
-//		Tablet,550.00
-
-//		Most expensive:
-//		IPhone, 910.00
-
-		Locale.setDefault(Locale.US);
-		List<Product> list = new ArrayList<>();
-
-		String path = "C:\\temp\\in.txt";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			String line = br.readLine();
-			while (line != null) {
-				String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-				line = br.readLine();
-			}
-
-			Product x = CalculationService.max(list);
-			System.out.println("Most expensive:");
-			System.out.println(x);
-
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
 		}
-
 	}
 }
