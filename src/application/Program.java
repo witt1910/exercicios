3a8294268c7038c402c0f616373f68e19c9f83f0
Wagner Witt <wittwizard@gmail.com>
2020-04-01 12:44:03 -0300

    14.06 Generics - Tipos curinga delimitados (bounded wildcard) - covariância e contravariância

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,37 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.05 Generics - Tipos curinga delimitados (bounded wildcard)
-		// Princípio get/put - contravariância
-		// List<? extends Number> list, por exemplo
-		// Permite realizar um Put, mas não um Get
-		// Não se sabe se o tipo do item a ser buscado é compatível
+		// 14.06 Generics - Tipos curinga delimitados (bounded wildcard)
+		// Covariância e contravariância
 
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		List<Object> myObjs = new ArrayList<Object>();
-		myObjs.add("Maria");
-		myObjs.add("Alex");
-		
-		List<? super Number> myNums = myObjs;
-		
-		myNums.add(10);
-		myNums.add(3.14);
-		
-		Number x = myNums.get(0); // erro de compilacao
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
+
+	// O primeiro argumento, extends, pode apenas ler*, e o faz
+	// O segundo argumento pode apenas inserir*, e o faz
+	// Talvez possam ser manuseados como tipos menos/mais abrangentes - vide printList()
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
+
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 }
