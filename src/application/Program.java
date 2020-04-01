96b0699b971ecfa17468278d8d864749b0fd330f
Wagner Witt <wittwizard@gmail.com>
2020-04-01 12:36:16 -0300

    14.04 Generics - Tipos curinga delimitados (bounded wildcard) - Princípio get/put - covariância

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,23 @@
 package application;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.03 Generics - Tipos curinga (wildcard types)
-		// O supertipo de qualquer tipo de lista é List<?> - e não List<Object>
-		
-		List<Integer> myInts = Arrays.asList(5, 2, 10);
-		printList(myInts);
-	}
+		// 14.04 Generics - Tipos curinga delimitados (bounded wildcard) - Princípio
+		// get/put - covariância
+		// List<? extends Number> list, por exemplo
+		// Permite realizar um Get, mas não um Put
+		// Não se sabe se o tipo do item a ser acrescido é compatível
 
-	public static void printList(List<?> list) {
-		for (Object obj : list) {
-			System.out.println(obj);
-		}
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(10);
+		intList.add(5);
+		List<? extends Number> list = intList;
+		Number x = list.get(0);
+
+		list.add(20); // erro de compilacao
 	}
 }
