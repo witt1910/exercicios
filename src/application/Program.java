30f4a0c5201c0bf87216fb087c6419d59e3e56b2
Wagner Witt <wittwizard@gmail.com>
2020-04-01 10:08:34 -0300

    14.02 Genericos delimitados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,55 @@
 package application;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-import model.services.PrintService;
+import model.entities.Product;
+import model.services.CalculationService;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.01 Generics
+		// 14.02 Genericos delimitados
 
-//		Deseja-se fazer um programa que leia uma quantidade N, e depois N números
-//		inteiros. Ao final, imprima esses números de forma organizada conforme
-//		exemplo. Em seguida, informar qual foi o primeiro valor informado.
-		
-		Scanner sc = new Scanner(System.in);
-		
-		PrintService<Integer> ps = new PrintService<>();
-		
-		System.out.print("How many values? ");
-		int n = sc.nextInt();
-		
-		for (int i = 0; i < n; i++) {
-			Integer value = sc.nextInt();
-			ps.addValue(value);
+//		Uma empresa de consultoria deseja avaliar a performance de produtos,
+//		funcionários, dentre outras coisas. Um dos cálculos que ela precisa é encontrar
+//		o maior dentre um conjunto de elementos. Fazer um programa que leia um
+//		conjunto de produtos a partir de um arquivo, conforme exemplo, e depois
+//		mostre o mais caro deles.
+
+//		in.txt
+//		Computer,890.50
+//		IPhone X,910.00
+//		Tablet,550.00
+
+//		Most expensive:
+//		IPhone, 910.00
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		String path = "C:\\temp\\in.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+
+			Product x = CalculationService.max(list);
+			System.out.println("Most expensive:");
+			System.out.println(x);
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
-		
-		ps.print();
-		Integer x = ps.first();
-		System.out.println("First: " + x);
-		
-		sc.close();
+
 	}
 }
