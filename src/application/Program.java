7677c5ae55cd790b6fe14e0dbe1dd4cd91e8971f
Wagner Witt <wittwizard@gmail.com>
2020-04-02 16:22:47 -0300

    14.11 Set - TreeSet (necessita implementação de Comparable<> para comparar)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,24 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import model.entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.10 Set - hashCode e equals (necessario para objs criados)
+		// 14.11 Set - TreeSet (necessita implementação de Comparable<> para comparar)
 
-		Set<Product> set = new HashSet<>();
+		Set<Product> set = new TreeSet<>();
 
 		set.add(new Product("TV", 900.0));
 		set.add(new Product("Notebook", 1200.0));
 		set.add(new Product("Tablet", 400.0));
 
-		Product prod = new Product("Notebook", 1200.0);
+		for (Product p : set) {
+			System.out.println(p);
+		}
 
-		// como ha hashCode e equals, se compara cada elemento
-		// e não a referência, que, no caso, daria diferente - false
-		System.out.println(set.contains(prod));
 	}
 }
--- src/model/entities/Product.java
+++ src/model/entities/Product.java
@@ -1,6 +1,6 @@
 package model.entities;
 
-public class Product {
+public class Product implements Comparable<Product> {
 	private String name;
 	private Double price;
 
@@ -54,6 +54,16 @@ public class Product {
 		} else if (!price.equals(other.price))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+
+	@Override
+	public int compareTo(Product other) {
+		return name.toUpperCase().compareTo(other.getName().toUpperCase());
 	}
 
 }
