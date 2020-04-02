aec5e18ce296b8b16f944bfdf7d689f48eae125b
Wagner Witt <wittwizard@gmail.com>
2020-04-02 16:34:28 -0300

    14.12 Set - Exercício 01 - HashSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,62 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
-import model.entities.Product;
+import model.entities.LogEntry;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.11 Set - TreeSet (necessita implementação de Comparable<> para comparar)
+		// 14.12 Set - Exercício 01 - HashSet
 
-		Set<Product> set = new TreeSet<>();
+//		Um site de internet registra um log de acessos dos usuários. Um
+//		registro de log consiste no nome de usuário (apenas uma palavra) e o
+//		instante em que o usuário acessou o site no padrão ISO 8601,
+//		separados por espaço, conforme exemplo. Fazer um programa que leia
+//		o log de acessos a partir de um arquivo, e daí informe quantos usuários
+//		distintos acessaram o site.
 
-		set.add(new Product("TV", 900.0));
-		set.add(new Product("Notebook", 1200.0));
-		set.add(new Product("Tablet", 400.0));
+//		input file:
+//		amanda 2018-08-26T20:45:08Z
+//		alex86 2018-08-26T21:49:37Z
+//		bobbrown 2018-08-27T03:19:13Z
+//		amanda 2018-08-27T08:11:00Z
+//		jeniffer3 2018-08-27T09:19:24Z
+//		alex86 2018-08-27T22:39:52Z
+//		amanda 2018-08-28T07:42:19Z
 
-		for (Product p : set) {
-			System.out.println(p);
+		Scanner sc = new Scanner(System.in);
+
+		Set<LogEntry> set = new HashSet<>();
+
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				set.add(new LogEntry(username, moment));
+
+				line = br.readLine();
+			}
+
+			System.out.println("Total users: " + set.size());
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
 
+		sc.close();
 	}
 }
