08d5da3bf966737d5021a8de1d0ec32f9d4492ff
Wagner Witt <wittwizard@gmail.com>
2020-04-02 18:48:01 -0300

    14.16 - Map - Exercício 01

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,33 +1,68 @@
 package application;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
-
-import model.entities.Product;
+import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.16 - Map - com objetos - comapra-se ou por ponteiro 
-		// ou por hashCode e equals
+		// 14.16 - Map - Exercício 01
 
-		// Pode-se usar objetos criados como chaves
-		Map<Product, Double> stock = new HashMap<>();
+//		Na contagem de votos de uma eleição, são gerados vários registros
+//		de votação contendo o nome do candidato e a quantidade de votos
+//		(formato .csv) que ele obteve em uma urna de votação. Você deve
+//		fazer um programa para ler os registros de votação a partir de um
+//		arquivo, e daí gerar um relatório consolidado com os totais de cada
+//		candidato.
+		
+//		input file example:
+//		Alex Blue,15
+//		Maria Green,22
+//		Bob Brown,21
+//		Alex Blue,30
+//		Bob Brown,15
+//		Maria Green,27
+//		Maria Green,22
+//		Bob Brown,25
+//		Alex Blue,31
 
-		// Criando-se objetos
-		Product p1 = new Product("Tv", 900.0);
-		Product p2 = new Product("Notebook", 1200.0);
-		Product p3 = new Product("Tablet", 400.0);
+		Scanner sc = new Scanner(System.in);
 
-		// Acrecendo-se os objetos
-		stock.put(p1, 10000.0);
-		stock.put(p2, 20000.0);
-		stock.put(p3, 15000.0);
+		Map<String, Integer> votes = new LinkedHashMap<>();
 
-		// Criando novo objto, igual ao p1, mas com outra referência/ponteiro
-		Product ps = new Product("Tv", 900.0);
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
 
-		// sem hashCode e equals, compara-se por ponteiros - e daria 'false'
-		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+
+				String[] fields = line.split(",");
+				String name = fields[0];
+				int count = Integer.parseInt(fields[1]);
+				if (votes.containsKey(name)) {
+					int votesSoFar = votes.get(name);
+					votes.put(name, count + votesSoFar);
+				} else {
+					votes.put(name, count);
+				}
+				line = br.readLine();
+			}
+
+			for (String s : votes.keySet()) {
+				System.out.println(s + ": " + votes.get(s));
+			}
+
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+			;
+		}
+
+		sc.close();
 	}
 }
