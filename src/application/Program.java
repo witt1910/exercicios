b42f49a54949eafaad88d86d6dbec542cc8240a6
Wagner Witt <wittwizard@gmail.com>
2020-04-01 21:23:51 -0300

    14.09 Set - union, intersection e difference

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,44 +1,31 @@
 package application;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.08 Set
-		// Conjunto que não admite repetições, elementos não possuem posição
-		// Acesso, isnerção e remoção rápidos
-		// Principais implementações:
-		// 		HashSet: rápido (O(1) em tabela hash), não ordenado;
-		// 		LinkedHashSet: vel. média, repeita ordem do add;
-		//		TreeSet: lento (O(log(n))), ordena pelo compareTo (do Comparable)
-		// 		ou Comparator
+		// 14.09 Set - union, intersection e difference
 
-		Set<String> set = new HashSet<>();
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
 
-		// add elementos
-		set.add("TV");
-		set.add("Notebook");
-		set.add("Tablet");
-		set.add("Fry");
-		set.add("Table");
-		set.add("Lamp");
+		// union
+		// A linha abaixo cria um conjunto c cópia de a;
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 
-		// remove elementos
-		set.remove("TV");
+		// intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
 
-		// remove se tal condição for atnedida
-		set.removeIf(x -> x.charAt(0) == 'T');
-
-		// indica se o conjunto contem ou nao tal elemento
-		System.out.println(set.contains("Notebook"));
-
-		// indica tamanho do conjunto
-		System.out.println("Size: " + set.size());
-
-		for (String p : set) {
-			System.out.println(p);
-		}
+		// difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 	}
 }
