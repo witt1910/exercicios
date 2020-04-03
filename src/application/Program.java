9524f54f66df3186a7b705f2523724aaaed55f15
Wagner Witt <wittwizard@gmail.com>
2020-04-02 21:32:03 -0300

    15.01 - Comparator (objeto de classe separada)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,68 +1,25 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
 
-		// 14.16 - Map - Exercício 01
+		// 15 - Comparator (objeto de classe separada)
 
-//		Na contagem de votos de uma eleição, são gerados vários registros
-//		de votação contendo o nome do candidato e a quantidade de votos
-//		(formato .csv) que ele obteve em uma urna de votação. Você deve
-//		fazer um programa para ler os registros de votação a partir de um
-//		arquivo, e daí gerar um relatório consolidado com os totais de cada
-//		candidato.
-		
-//		input file example:
-//		Alex Blue,15
-//		Maria Green,22
-//		Bob Brown,21
-//		Alex Blue,30
-//		Bob Brown,15
-//		Maria Green,27
-//		Maria Green,22
-//		Bob Brown,25
-//		Alex Blue,31
+		List<Product> list = new ArrayList<>();
 
-		Scanner sc = new Scanner(System.in);
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
 
-		Map<String, Integer> votes = new LinkedHashMap<>();
+		list.sort(new MyComparator());
 
-		System.out.print("Enter file full path: ");
-		String path = sc.nextLine();
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			String line = br.readLine();
-			while (line != null) {
-
-				String[] fields = line.split(",");
-				String name = fields[0];
-				int count = Integer.parseInt(fields[1]);
-				if (votes.containsKey(name)) {
-					int votesSoFar = votes.get(name);
-					votes.put(name, count + votesSoFar);
-				} else {
-					votes.put(name, count);
-				}
-				line = br.readLine();
-			}
-
-			for (String s : votes.keySet()) {
-				System.out.println(s + ": " + votes.get(s));
-			}
-
-		} catch (IOException e) {
-			System.out.println("Erro: " + e.getMessage());
-			;
+		for (Product p : list) {
+			System.out.println(p);
 		}
-
-		sc.close();
 	}
 }
