f8ddf70af2880d78bb406e122a9a0702479ecc97
Wagner Witt <wittwizard@gmail.com>
2020-05-31 20:54:06 -0300

    15.06 - Predicate (expressão lambda inline)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,60 +1,31 @@
 package application;
 
 import java.text.ParseException;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Product;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
 
-		// 14.14b Set - Exercício 02 - HashSet e addAll()
-
-//		Em um portal de cursos online, cada usuário possui um código único, 
-//		representado por um número inteiro.
-//		Cada instrutor do portal pode ter vários cursos, sendo que um mesmo aluno pode se
-//		matricular em quantos cursos quiser. Assim, o número total de alunos de um instrutor não
-//		é simplesmente a soma dos alunos de todos os cursos que ele possui, pois pode haver
-//		alunos repetidos em mais de um curso.
-//		O instrutor Alex possui três cursos A, B e C, e deseja saber seu número total de alunos.
-//		Seu programa deve ler os alunos dos cursos A, B e C do instrutor Alex, depois mostrar a
-//		quantidade total e alunos dele, conforme exemplo.
-
+		// 15.06 - Predicate (expressão lambda inline)
+		// Fazer um programa que, a partir de uma lista de produtos, remova da
+		// lista somente aqueles cujo preço mínimo seja 100.
 		
-		Scanner sc = new Scanner(System.in);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
 
-		Set<Integer> courseA = new HashSet<>();
-		Set<Integer> courseB = new HashSet<>();
-		Set<Integer> courseC = new HashSet<>();
-		Set<Integer> totalStudents = new HashSet<>();
-
-		System.out.print("How many students for course A? ");
-		int std = sc.nextInt();
-		addGrade(courseA, std);
-
-		System.out.print("How many students for course B? ");
-		std = sc.nextInt();
-		addGrade(courseB, std);
-
-		System.out.print("How many students for course C? ");
-		std = sc.nextInt();
-		addGrade(courseC, std);
-
-		totalStudents.addAll(courseA);
-		totalStudents.addAll(courseB);
-		totalStudents.addAll(courseC);
-
-		System.out.println("Total students: " + totalStudents.size());
-
-		sc.close();
-	}
-
-	public static void addGrade(Set<Integer> list, int std) {
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < std; i++) {
-			int grade = sc.nextInt();
-			list.add(grade);
+		double min = 100;
+		
+		list.removeIf(p -> p.getPrice() >= min);
+		
+		for (Product p : list) {
+			System.out.println(p);
 		}
-		sc.close();
 	}
 }
