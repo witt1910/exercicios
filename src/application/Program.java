85b2cae2e076c1243369dfb6ffc4807efc7bc9c7
Wagner Witt <wittwizard@gmail.com>
2020-05-31 20:55:11 -0300

    15.07 - Predicate (expressão lambda inline)

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,21 +9,19 @@ import model.entities.Product;
 public class Program {
 	public static void main(String[] args) throws ParseException {
 
-		// 15.06 - Predicate (expressão lambda inline)
-		// Fazer um programa que, a partir de uma lista de produtos, remova da
-		// lista somente aqueles cujo preço mínimo seja 100.
-		
+		// 15.07 - Predicate (expressão lambda inline)
+		// Fazer um programa que, a partir de uma lista de produtos, aumente o
+		// preço dos produtos em 10%
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		double min = 100;
-		
-		list.removeIf(p -> p.getPrice() >= min);
-		
+		list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
+
 		for (Product p : list) {
 			System.out.println(p);
 		}
