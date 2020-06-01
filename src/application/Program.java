2a3199d99bcba2fe9ec96d6c10f56741ddefaf95
Wagner Witt <wittwizard@gmail.com>
2020-05-31 21:08:29 -0300

    15.09 - Lambda - Funções recebendo funções como argumento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,21 @@
 package application;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Locale;
 
 import model.entities.Product;
+import model.services.ProductService;
 
 public class Program {
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
-		// 15.08 - Function - map (expressão lambda inline)
-		// Fazer um programa que, a partir de uma lista de produtos, gere uma
-		// nova lista contendo os nomes dos produtos em caixa alta.
+		// 15.09 - Lambda - Funções recebendo funções como argumento
+		// Fazer um programa que, a partir de uma lista de produtos, calcule a
+		// soma dos preços somente dos produtos cujo nome começa com "T".
 
+		Locale.setDefault(Locale.US);
+		
 		List<Product> list = new ArrayList<>();
 
 		list.add(new Product("Tv", 900.00));
@@ -21,9 +23,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		double sum = ProductService.sum(list, p -> p.getName().charAt(0) == 'T');
 		
-		names.forEach(System.out::println);
+		System.out.println("Sum: " + String.format("%.2f", sum));
 
 	}
 }
